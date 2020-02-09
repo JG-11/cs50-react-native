ea39147a5266156922ec7d15584595d31f9ecb0d
Genaro Almaraz <jg08950@gmail.com>
2020-02-09 12:27:34 -0600

    Browser and Node.js Global Objects

---
+++ lecture-0/GlobalObject.js
@@ -0,0 +1,19 @@
+/*
+    All variables and functions are actually parameters and methods
+    on the global object.
+
+    Browser global object is the 'window' object
+    Node.js global object is the 'global' object
+*/
+
+console.log(global)
+
+/*
+    On a browser:
+    
+    window 
+
+    var x = 50
+
+    window.x
+*/
