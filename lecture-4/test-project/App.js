6b8c2a925f5319e889ce4849a2cb3afb14f76253
Genaro Almaraz <jg08950@gmail.com>
2020-03-09 21:49:58 -0600

    Flat list

--- lecture-4/test-project/App.js
+++ lecture-4/test-project/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
-import contacts from './contacts';
-import Row from './Row.js';
+import contacts, {compareNames} from './contacts';
+import Row from './Row';
 
 export default class App extends React.Component {
   state = {
     showContacts: false,
+    contacts: contacts
   }
 
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  renderItem = obj => <Row {...obj.item}/>
+
+  sort = () => {
+    this.setState(prevState => ({
+      //Immutability is important
+      contacts: [...prevState.contacts].sort(compareNames)
+    }))
+  } 
+
   render() {
     return (
       <View style={styles.container}>
         <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort" onPress={this.sort} />
         {this.state.showContacts && (
-          <ScrollView>
-            {contacts.map(contact => <Row {...contact}/>)}
-          </ScrollView>
+          <FlatList
+            data={this.state.contacts}
+            renderItem={this.renderItem}
+          />
         )}
       </View>
     );
