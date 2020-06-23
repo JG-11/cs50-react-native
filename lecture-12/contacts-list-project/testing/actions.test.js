9622a23f8b1ae00ce17db0e6c51d38ce3c1ef474
Genaro Almaraz <jg08950@gmail.com>
2020-06-22 23:18:12 -0500

    Group tests together: Jest

--- lecture-12/contacts-list-project/testing/actions.test.js
+++ lecture-12/contacts-list-project/testing/actions.test.js
@@ -1,5 +1,15 @@
 import * as actions from './../redux/actions'
+ 
+describe('updateUser returns an action', () => {
+  it('handles name', () => {
+    expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot()
+  })
 
-test('updateUser returns an action', () => {
-  expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot()
+  it('handles empty name', () => {
+    expect(actions.updateUser({ name: '' })).toMatchSnapshot()
+  })
+
+  it('handles empty object', () => {
+    expect(actions.updateUser({})).toMatchSnapshot()
+  }) 
 })
