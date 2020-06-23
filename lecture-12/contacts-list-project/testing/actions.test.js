d7ef34921e13d2deca80f2b1021512f9fe12a341
Genaro Almaraz <jg08950@gmail.com>
2020-06-22 22:55:16 -0500

    Snapshots: Jest and Redux

--- lecture-12/contacts-list-project/testing/actions.test.js
+++ lecture-12/contacts-list-project/testing/actions.test.js
@@ -1,13 +1,5 @@
 import * as actions from './../redux/actions'
 
 test('updateUser returns an action', () => {
-  expect(actions.updateUser({ name: 'test name' })).toEqual({
-	  type: actions.UPDATE_USER,
-	  payload: { name: 'test name'}
-  })
-  
-  expect(actions.updateUser({ phone: '1234' })).toEqual({
-          type: actions.UPDATE_USER,
-	  payload: { phone: '1234' }
-  })
+  expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot()
 })
