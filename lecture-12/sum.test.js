7cd48d9e50d8531a9247dd5dd81b6dc309b0f17f
Genaro Almaraz <jg08950@gmail.com>
2020-06-22 20:04:30 -0500

    Unit testing

---
+++ lecture-12/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./sum.js')
+
+console.assert(sum(1, 1) === 2, 'Error summing 1 and 1')
+console.assert(sum(3, 2) === 5, 'Error summing 3 and 2')
+console.assert(sum(400, 900) === 1300, 'Error summing 400 and 900')
